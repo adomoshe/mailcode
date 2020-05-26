892044021ada34e7436f2930f67e25fe0130a6f0
Ado Moshe <adoran@mykumbaya.com>
2020-05-25 18:33:55 -0700

    cleaning up file structure and adding types to npm package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const zipcodes = require('./assets/zipcodes.json');
+import * as zipcodes from '../assets/zipcodes.json';
 
 const handleError = (error: string): any => {
   console.warn(error)
@@ -11,21 +11,22 @@ const checkZipType = (zip: string | number): string => {
     return zip.toString();
   } else {
     handleError('zip can be one of (string|number)');
+    return '';
   }
 }
 
 const getLocationFromZip = (zip: string | number): object => {
-  let location: object;
-  let zipString: string;
-
   if (!zip) {
     return handleError('Please enter a zip (string|number)');
   }
-  zipString = checkZipType(zip);
+  const zipString: string = checkZipType(zip);
+  if (!zipString) {
+    return {};
+  }
   if (zipString.length !== 5) {
     return handleError('Please enter a 5 digit zip (string|number)')
   }
-  location = zipcodes[zip];
+  const location: object = zipcodes[zip];
   if (!location) {
     return handleError(`Location for zip '${zip}' was not found`);
   }
